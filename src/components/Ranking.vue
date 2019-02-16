4694ed371ea965cff8e4a13bd0874a7a6658c49f
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-16 21:43:36 -0200

    passing MainView and creating Ranking component

---
+++ src/components/Ranking.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="ranking-container">
+    <div class="img-container">
+      <img src="@/assets/logo_matchbox_amarelo.png"/>
+    </div>
+    <div class="header">
+      <span class="title">
+        ranking
+      </span>
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ranking',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.ranking-container {
+
+  .img-container {
+    text-align: left;
+  }
+
+  .header {
+    font-size: 30pt;
+    font-weight: bolder;
+    background-color: #fff;
+    border-radius: 5px;
+  }
+
+  .content {
+    border-radius: 5px;
+    background-color: #fff;
+    padding: 10px 0px;
+    margin: 10px 0;
+  }
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="content-container">
+     <Ranking :msg="Ranking"/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .home {
+    width: 100%;
+    height: 100vh;
+    display: block;
+    background-color: #453995;
+    .content-container {
+      max-width: 342px;
+      margin: 0 auto;
+    }
+  }
+</style>
+
+<script>
+import Ranking from '@/components/Ranking'
+
+export default {
+  name: 'MainView',
+  components: {
+    Ranking
+  }
+}
+</script>
