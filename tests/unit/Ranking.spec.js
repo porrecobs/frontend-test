73b177a3a3542fe633673a23d31dd6b36bf63d18
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-17 11:10:16 -0300

    created test to Ranking component

---
+++ tests/unit/Ranking.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount } from '@vue/test-utils'
+import Ranking from '@/components/Ranking'
+import ProfileCard from '@/components/ProfileCard'
+const userListFixt = require('../../matchboxbrasil.json')
+
+describe('Ranking', () => {
+  let props
+
+  const build = () => {
+    const wrapper = shallowMount(Ranking, {
+      propsData: props
+    })
+
+    return {
+      wrapper,
+      ProfileCard: () => wrapper.find(ProfileCard)
+    }
+  }
+
+  beforeEach(() => {
+    props = {
+      userList: userListFixt
+    }
+  })
+
+  it('renderiza o componente', () => {
+    const { wrapper } = build()
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renderiza os componentes filhos', () => {
+    const { ProfileCard } = build()
+
+    expect(ProfileCard().exists()).toBe(true)
+  })
+})
