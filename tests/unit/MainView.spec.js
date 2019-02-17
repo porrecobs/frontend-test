46b4f3fdc2dcdba15a759c5be8e11e6653e192f9
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-16 23:02:26 -0200

    bind test between MainView e Ranking component

--- tests/unit/MainView.spec.js
+++ tests/unit/MainView.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import MainView from '@/views/MainView'
+import Ranking from '@/components/Ranking'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -8,11 +9,15 @@ localVue.use(Vuex)
 describe('MainView', () => {
   const build = () => {
     const wrapper = shallowMount(MainView, {
-      localVue
+      localVue,
+      data: () => ({
+        userList: {}
+      })
     })
 
     return {
-      wrapper
+      wrapper,
+      ranking: () => wrapper.find(Ranking)
     }
   }
 
@@ -20,5 +25,22 @@ describe('MainView', () => {
     const { wrapper } = build()
 
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renderiza os componentes filhos', () => {
+    const { ranking } = build()
+
+    expect(ranking().exists()).toBe(true)
+  })
+
+  it('passa a lista para o componente de ranking', () => {
+    const { wrapper, ranking } = build()
+    wrapper.setData({
+      userList: {
+        name: 'Nicolas'
+      }
+    })
+
+    expect(ranking().vm.userList).toBe(wrapper.vm.userList)
   })
 })
