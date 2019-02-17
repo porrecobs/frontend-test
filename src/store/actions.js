42b3ac07e3c3c2dc58b9f9110f5c2f52ea68d89e
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-17 12:01:46 -0300

    creating SEARCH_USER_LIST function

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,16 @@
-export default {}
+import api from '@/api'
+
+export default {
+  // eslint-disable-next-line no-empty-pattern
+  SEARCH_USER_LIST ({ commit }, {}) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userList = await api.searchUserList()
+        commit('SET_USER', userList)
+        resolve(userList)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+}
