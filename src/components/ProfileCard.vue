fbb33ebc821b215a727bd673726dd7d5af795b94
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-16 23:02:49 -0200

    ProfileCard component created

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card-container">
+    Profile
+  </div>
+</template>
+<style lang="sass" scoped>
+
+</style>
+<script>
+export default {
+  name: 'ProfileCard',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
