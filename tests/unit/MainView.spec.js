b7998a2bc43534239c0cb4119247c24889d69033
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-16 23:38:43 -0200

    refactoring to state de binding object

--- tests/unit/MainView.spec.js
+++ tests/unit/MainView.spec.js
@@ -2,17 +2,19 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import MainView from '@/views/MainView'
 import Ranking from '@/components/Ranking'
+import initState from '@/store/state'
+const userListFixt = require('../../matchboxbrasil.json')
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
 describe('MainView', () => {
+  let state
+
   const build = () => {
     const wrapper = shallowMount(MainView, {
       localVue,
-      data: () => ({
-        userList: {}
-      })
+      store: new Vuex.Store({ state })
     })
 
     return {
@@ -20,6 +22,10 @@ describe('MainView', () => {
       ranking: () => wrapper.find(Ranking)
     }
   }
+
+  beforeEach(() => {
+    state = { ...initState }
+  })
 
   it('renderiza o componente', () => {
     const { wrapper } = build()
@@ -34,13 +40,9 @@ describe('MainView', () => {
   })
 
   it('passa a lista para o componente de ranking', () => {
-    const { wrapper, ranking } = build()
-    wrapper.setData({
-      userList: {
-        name: 'Nicolas'
-      }
-    })
+    const { ranking } = build()
+    state = { userList: userListFixt }
 
-    expect(ranking().vm.userList).toBe(wrapper.vm.userList)
+    expect(ranking().vm.userList).toBe(state.userList)
   })
 })
