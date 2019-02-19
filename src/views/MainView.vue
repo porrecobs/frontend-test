3a9899eb986949e80c6c2aff604596aa2d074a0b
Nicolas Pereira <nicolaspereira@MacBook-Air-de-Nicolas.local>
2019-02-19 19:29:44 -0300

    sorting userList

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -12,11 +12,22 @@
     height: 100vh;
     display: block;
     background-color: #453995;
+
     .content-container {
-      max-width: 342px;
+      width: 100%;
       margin: 0 auto;
+      position: absolute;
+      top: 50%; 
+      left: 50%;
+      transform: translate(-50%,-50%);
     }
   }
+@media only screen and (min-width: 482px) {
+  .content-container {
+    max-width: 342px;
+  }
+}
+
 </style>
 
 <script>
@@ -30,15 +41,35 @@ export default {
   },
   computed: {
     ...mapState({
-      userList: 'userList',
+      userList: 'userList'
     })
   },
-  created() {
-    this.getUsers()
+  mounted: function () {
+    this.$store.dispatch('SEARCH_USER_LIST', { })
+      .then(res => {
+        this.sortByPositive(res.data)
+      })
+      .catch(err => err)
   },
   methods: {
-    getUsers () {
-      this.$store.dispatch('SEARCH_USER_LIST', { })
+    async sortByPositive(userList) {
+      await userList.map((elem) => {
+        this.getPercentages(elem)
+      })
+      userList = this.sortUsers(userList)
+    },
+    sortUsers (userList) {
+      return userList.sort((a, b) => {
+        return (a.positive > b.positive) ? -1 : ((b.positive > a.positive) ? 1 : 0)
+      })
+    },
+    getPercentages (user) {
+      const total = user.positive + user.negative
+      const positive = Math.floor(user.positive / total * 100)
+      const negative = Math.floor(user.negative / total * 100)
+      user.positive = positive
+      user.negative = negative
+      return user
     }
   }
 }
